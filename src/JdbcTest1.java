14ac7bbbad73fb55a0ef73680f809dd91fec39c7
leojsmada <joeladams@bellsouth.net>
2020-08-07 18:58:16 -0400

    Java dataBase take one...

---
+++ src/JdbcTest1.java
@@ -0,0 +1,26 @@
+//  Establish a connection to an Interbase database using JDBC and ODBC.
+import java.sql.*;
+
+class JdbcTest1
+{
+    public static void main(String[] args)
+    {
+        try {
+            String url = "jdbc:mysql://localhost/";
+            Connection conn = DriverManager.getConnection(url, "root", "bupesap2");
+            Statement st = conn.createStatement();
+
+
+             st.executeUpdate("INSERT INTO students (name, age, address, belt) VALUES " +
+                     "(JohnSmith, 17, '560 Smith Lane', 'blue')");
+
+            conn.close();
+        }
+        catch (Exception e)
+        {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+
+    }
+}
