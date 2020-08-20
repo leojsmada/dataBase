6574b6949c40db9480b2f1c7edf8a4b8dd3b6b48
leojsmada <joeladams@bellsouth.net>
2020-08-20 00:57:50 -0400

    NOW WORKING ON GETTING Field 'idstudents' doesn't have a default value ERROR WORKED OUT.

--- src/JdbcTest1.java
+++ src/JdbcTest1.java
@@ -7,13 +7,15 @@ class JdbcTest1
     {
         try
         {
-            //String url = "jdbc:mysql://root:3306/";
-            String url = "jdbc:mysql://localhost/";
+            //String url = "127.0.0.1";
+
+
+            String url = "jdbc:mysql://localhost:3306/mysql";
 
             Connection conn = DriverManager.getConnection(url, "root", "bupesap2");
             Statement st = conn.createStatement();
-//-------//THIS STATEMENT BELOW IS THROWING A "No database selected" ERROR//---------------//
-          st.executeUpdate("INSERT INTO students (name, age, address, belt) VALUES " +
+//-------//THIS STATEMENT BELOW IS THROWING A " Field 'idstudents' doesn't have a default value" ERROR//---------------//
+          st.executeUpdate("INSERT INTO students.students (name, age, address, belt) VALUES " +
                      "('John Smith', 17, '560 Smith Lane', 'blue')");
 
             conn.close();
